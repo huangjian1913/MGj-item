d81b5101396151ca690e4786d63544f74d8f8ada
huangjian1913 <hj6321567@163.com>
2020-06-06 17:43:15 +0800

    星期6

--- src/script/index.js
+++ src/script/index.js
@@ -1,27 +1,167 @@
-// const head_aboutus = document.querySelector('.header-aboutus');
-// const head_aboutusbox = document.querySelector('.header-aboutus-box');
-// head_aboutus.onmouseover = function() {
-//     head_aboutusbox.style.display = "block";
-// }
+! function() {
+    const aboutus = document.querySelector('.header-nav-content .header-aboutus');
+    const aboutusbox = document.querySelector('.header-nav-content .header-abouts-box');
+    const mulu = document.querySelector('.header-wrap .mulu span')
+    const mululist = document.querySelector('.header-wrap .mulu .mulu-list');
+    const advertising = document.querySelector('.advertising');
+    const deletebtn = document.querySelector('.advertising .content .delete');
 
-window.onload = function() {
-    const djs_hour = document.querySelector(".countdown .hour");
-    const djs_min = document.querySelector(".countdown .min");
-    const djs_second = document.querySelector(".countdown .second");
+    aboutus.onmousemove = aboutusbox.onmousemove = function() {
+        aboutusbox.style.display = "block";
+    }
+    aboutus.onmouseout = aboutusbox.onmouseout = function() {
+        aboutusbox.style.display = "none";
+    }
+    mulu.onmousemove = mululist.onmousemove = function() {
+        mululist.style.display = "block";
+    }
+    mulu.onmouseout = mululist.onmouseout = function() {
+        mululist.style.display = "none";
+    }
+    deletebtn.onclick = function() {
+        advertising.style.display = "none";
+    }
+}()
 
-    setInterval(function() {
-        var futureTime = new Date(2020, 5, 4, 18, 0, 0); // 未来时间
-        var now = Date.now();
-        var time = futureTime - now; // 需要倒计时的时间 毫秒
-        var s = time / 1000; // 将毫秒转换成秒
-        var day = Math.floor(s / (60 * 60 * 24)); // 将秒换算成天
-        var hour = Math.floor(s % 86400 / 3600); // 计算剩余小时
-        var min = Math.floor(s % 3600 / 60); // 计算分钟
-        var sec = Math.floor(s % 60); // 获得秒
-        // djs.innerHTML = '距离春节假期来临 还剩:' + day + '天' + hour + '时' + min + '分' + sec + '秒';
-        djs_hour.innerHTML = hour;
-        djs_min.innerHTML = min;
-        djs_second.innerHTML = sec;
-    }, 1000);
+//蘑菇街搜索引擎： https: //list.mogu.com/search?cKey=pc-search-entry&_version=8388605&searchKey=bbb&callback=hehe&_=1591427175916
+const search = document.querySelector('.header .header-wrap .search input');
+const search_box = document.querySelector('.header .header-wrap .search .search_box');
 
+function database(data) {
+    let arr = data.result.list;
+    arr.shift();
+    let strhtml = '';
+    for (let value of arr) {
+        strhtml += `<li>
+        <a href="https://list.mogu.com/search/goods?q=${value.query}&title=${value.query}">${value.query}</a>
+        </li>`
+        if (value.query == undefined) {
+            strhtml = '';
+        }
+    }
+    search_box.innerHTML = strhtml;
 }
+search.oninput = function() {
+    let cs = document.querySelector('#hehe');
+    if (cs) {
+        document.body.removeChild(cs);
+    }
+    let cScript = document.createElement('script');
+    cScript.id = 'hehe';
+    cScript.src = 'https://list.mogu.com/search?cKey=pc-search-entry&_version=8388605&searchKey=' + this.value + '&callback=database&_=1591427175916';
+    document.body.appendChild(cScript);
+}
+
+
+! function() {
+    window.onload = function() {
+        const djs_hour = document.querySelector(".countdown .hour");
+        const djs_min = document.querySelector(".countdown .min");
+        const djs_second = document.querySelector(".countdown .second");
+
+        setInterval(function() {
+            var futureTime = new Date(2020, 5, 6, 18, 0, 0); // 未来时间
+            var now = Date.now();
+            var time = futureTime - now; // 需要倒计时的时间 毫秒
+            var s = time / 1000; // 将毫秒转换成秒
+            var day = Math.floor(s / (60 * 60 * 24)); // 将秒换算成天
+            var hour = Math.floor(s % 86400 / 3600); // 计算剩余小时
+            var min = Math.floor(s % 3600 / 60); // 计算分钟
+            var sec = Math.floor(s % 60); // 获得秒
+            // djs.innerHTML = '距离春节假期来临 还剩:' + day + '天' + hour + '时' + min + '分' + sec + '秒';
+            djs_hour.innerHTML = hour;
+            djs_min.innerHTML = min;
+            djs_second.innerHTML = sec;
+        }, 1000);
+
+    }
+}()
+
+! function($) {
+    const $banner = $('.body_wrap .outerbox .bannerbox ul');
+    let index = 1;
+    let timer = null;
+
+    function ullunbo1() {
+        if (index == $banner.length) {
+            index = 0;
+        }
+        $banner.eq(index).animate({
+            opacity: 1,
+        }).siblings('.body_wrap .outerbox .bannerbox ul').animate({
+            opacity: 0,
+        });
+    }
+    timer = setInterval(function() {
+        ullunbo1();
+        index++;
+    }, 2000)
+    $banner.hover(function() {
+        clearInterval(timer);
+    }, function() {
+        timer = setInterval(function() {
+            ullunbo1();
+            index++;
+        }, 2000);
+    })
+}(jQuery)
+
+! function($) {
+    const $bannerbox = $('.body_wrap .girl_close .bannerpic');
+    const $btnleft = $('.body_wrap .girl_close .bannerpic>p:eq(0)');
+    const $btnright = $('.body_wrap .girl_close .bannerpic>p:eq(1)');
+    const $ulbox = $('.body_wrap .girl_close .bannerpic>ul');
+    const $liboxs = $('.body_wrap .girl_close .bannerpic>ul>li');
+    let index = 0;
+    let timer = null;
+    let $clonebox1 = $liboxs.eq(0).clone(true, true);
+    let $clonebox2 = $liboxs.eq(1).clone(true, true);
+    let $clonebox3 = $liboxs.eq(2).clone(true, true);
+    let $clonebox4 = $liboxs.eq(3).clone(true, true);
+    let $bannerwidth = $bannerbox.width(); //追加
+    $ulbox.append($clonebox1).append($clonebox2).append($clonebox3).append($clonebox4).css({
+        width: $bannerwidth * 3
+    });
+    $bannerbox.hover(function() {
+        $btnright.show();
+        $btnleft.show();
+        clearInterval(timer);
+    }, function() {
+        $btnright.hide();
+        $btnleft.hide();
+        timer = setInterval(function() {
+            tabswitch();
+        }, 2000)
+    })
+
+
+    $btnright.on('click', function() {
+        tabswitch();
+    })
+    $btnleft.on('click', function() {
+        index -= 2;
+        tabswitch();
+    })
+
+    function tabswitch() {
+        index++;
+        if (index > 2) {
+            $ulbox.css({
+                left: 0,
+            })
+            index = 1;
+        }
+        if (index < 0) {
+            $ulbox.css({
+                left: -$bannerwidth * 2,
+            })
+            index = 1;
+        }
+        $ulbox.stop(true).animate({
+            left: -$bannerwidth * index,
+        })
+    }
+    timer = setInterval(function() {
+        tabswitch();
+    }, 2000)
+}(jQuery)
--- src/script/list.js
+++ src/script/list.js
@@ -1,25 +1,35 @@
-const goods_content = document.querySelector('.body_wrap .goods_content');
-console.log(goods_content);
-$ajax({
-    url: 'http://10.31.162.38/Mogujie/php/alldata.php',
-}).then(function(data) {
-    let arr = JSON.parse(data);
-    let strhtml = '';
-    for (let value of arr) {
-        strhtml += `  
-        <a href="detail.html" class="goods">
-            <div class="box"><img src="${value.url}" alt="">
-            </div>
-            <p>${value.title}</p>
-            <div class="price">
-                <span class="iconfont icon-renminbi"></span>
-                <span>${value.price}</span>
-                <span class="iconfont icon-renminbi"></span>
-                <span>${value.oldprice}</span>
-                <span class="iconfont icon-shoucang"></span>
-                <span>${value.sailnumber}</span>
-            </div>    
-        </a>`;
-    }
-    goods_content.innerHTML = strhtml;
-});
+! function() {
+    const $goods_content = $('.body_wrap .goods_content');
+    $.ajax({
+        url: 'http://10.31.162.38/Mogujie/php/alldata.php',
+        success: (function(data) {
+            let arr = JSON.parse(data);
+            let strhtml = '';
+            // console.log(arr);
+            $.each(arr, function(index, value) {
+                strhtml += `  
+                <a href="detail.html" class="goods" target="_blank">
+                    <div class="box"><img class="lazy" data-original="${value.url}" alt="">
+                    </div>
+                    <p>${value.title}</p>
+                    <div class="price">
+                        <span class="iconfont icon-renminbi"></span>
+                        <span>${value.price}</span>
+                        <span class="iconfont icon-renminbi"></span>
+                        <span>${value.oldprice}</span>
+                        <span class="iconfont icon-shoucang"></span>
+                        <span>${value.sailnumber}</span>
+                    </div>    
+                    <p>找相似</p>
+                </a>`;
+            })
+            $goods_content.html(strhtml);
+            // $(function() { //和拼接的元素放在一起。
+            $("img.lazy").lazyload({
+                effect: "fadeIn" //图片显示方式
+            });
+            // });
+        })
+    });
+
+}()
